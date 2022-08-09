6066487af0eb91c59a1fe77712f3b2354c45de14
JakeTurner616 <88468353+JakeTurner616@users.noreply.github.com>
2022-08-08 21:56:39 -0600

    Update foo.js

--- js/foo.js
+++ js/foo.js
@@ -1,9 +1,1 @@
-var req = new XMLHttpRequest();
-
-req.open('GET', 'proxy.php?url=https://tb.serverboi.org', false);
-req.send(null);
-
-if(req.status == 200) {
-   alert(req.responseText);
-}
-
+alert("Hello World");
