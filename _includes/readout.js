90a7c5de93f43fc9f39f2dec80d165a3a41467a2
JakeTurner616 <jake@serverboi.org>
2022-08-18 06:27:23 -0600

    Update readout.js

--- _includes/readout.js
+++ _includes/readout.js
@@ -1,6 +1,6 @@
-    //This file is not sourced in a script tag.
-    //This file, when hiding the iframe should be placed in the beginning of a body tag to run before page content of said iframe's host page.
-    //This is to lessen the load on the API serving disk usage.
+//this file is not sourced in a script tag
+//this file, when hiding the iframe should be placed in the beginning of a body tag to run before page content of said iframe's host page
+//this is to lessen the load on the API serving disk usage
     $(document).ready(function(){
       $("#iframe2").hide();
       $("#Link").click(function(){
@@ -10,8 +10,9 @@
       });
   });
 if (confirm("Before proceeding, the user must acknowledge that the following page contains programs and software that are either old/unmaintained or are no longer in active development:\nFiles are strictly for academic/curiosity use only, and are not in any way intended to be shared or resourced in either development within projects, or by means of infringing proprietary software and its copy-prevention.")) {
-    //USER AGREES
+//user agrees, do nothing
   } else {
+//return home
     window.location.href = "/";
   } 
 
